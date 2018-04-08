ef8e3afb207cbc91f3430d29dcc4aa54b63deebf
kirikira <kiri_so@outlook.com>
2018-04-08 14:59:12 +0800

    update

--- link/spiders/addresssplit.py
+++ link/spiders/addresssplit.py
@@ -5,12 +5,12 @@ def cut(address, tup):
     for item in tup:
         if len(address.split(item, 1)) > 1:
             return address.split(item, 1)[0] + item, address.split(item, 1)[1]
-
+    return address, address
 
 def address_split(address):
     try:
         first_class, second = cut(address, ('省', '自治区', '行政区', '市'))
-        second_class, third_class = cut(second, ('市', '行政单位', '州'))
+        second_class, third_class = cut(second, ('市', '行政单位', '州', '地区'))
         return first_class, second_class, third_class
     except TypeError:
         return address, '', ''
